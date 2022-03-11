8d39c89d10f3e12504bbd1bf41dcac9b3158b86b
iqrokristiadi <iqro.kristiadi2004@gmail.com>
2022-03-11 20:57:37 +0700

    add slider, style.css, image folder

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@
 // menampilkan sidebar
 const sideNav = document.querySelectorAll('.sidenav');
 M.Sidenav.init(sideNav)
+
+// slider
+
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+	indicators: 'false',
+	height: 500,
+	duration: 700,
+	interval: 5000
+});
