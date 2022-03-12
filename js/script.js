84df09f1975a8374a26210b98a4ab50f27b7f69c
iqrokristiadi <iqro.kristiadi2004@gmail.com>
2022-03-12 15:55:14 +0700

    scrollspy

--- js/script.js
+++ js/script.js
@@ -20,3 +20,12 @@ M.Parallax.init(parallax);
 
 const materialbox = document.querySelectorAll('.materialboxed');
 M.Materialbox.init(materialbox);
+
+// scrollspy
+
+const scroll = document.querySelectorAll('.scrollspy');
+
+M.ScrollSpy.init(scroll, {
+	scrollOffset: 50
+});
+
